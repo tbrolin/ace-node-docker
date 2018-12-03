dcf62edb6c25d34f37c5077559fff76895ca7e6c
Tobbe <tobias.brolin@atex.com>
2018-12-03 11:40:42 +0100

    require anything

--- index.js
+++ index.js
@@ -5,45 +5,33 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 } else {
-
   const net = require ('net');
   const vm = require ('vm');
   const parser = require ('ldjson-stream');
   const os = require('os');
-  
-  const slugify = require('slugify');
-  
-  const modules = {
-    slugify: require('slugify')
-  }
-  
-  let processed = 0;
-  
+
   const server = net.createServer ((c) => {
     const input = parser();
-    const output = parser.serialize();
-  
+
     c.on ('error', (error) => { console.log('CONNECTION ERROR', error);});
     input.on('error', (error) => { console.log('INPUT ERROR:', error); });
-    output.on('error', (error) => { console.log('OUTPUT ERROR:',error); });
+
     input.on('data', (job) => {
-  
-      console.error (`Processed ${processed} jobs`);
       processJob (job).then ((result) => {
         c.write(JSON.stringify(result) + os.EOL);
-        processed++;
       }).catch((err) => {
         console.log ('processing error', err);
-        c.write(JSON.stringify({ error: err.message }));
+        c.write(JSON.stringify({ error: err.message }) + os.EOL);
       });
     });
+
     c.pipe(input);
   });
-  
+
   server.listen (5555, () => {
     console.error ('Server listening on ', server.address ());
   });
-  
+
   function processJob ({ scripts, content }) {
     try {
       const callbacks = compile (scripts);
@@ -52,23 +40,22 @@ if (cluster.isMaster) {
       return Promise.reject(error);
     }
   }
-  
+
   function compile (scripts) {
     return scripts.map ((code) => {
       let context = {
-        require: (name) => {
-          return modules[name];
-        }
+        setTimeout,
+        require
       };
       vm.runInNewContext (code, context);
       return context.preStore;
     });
   }
-  
+
   function executor ({ callbacks, content }) {
     return callbacks.reduce ((p, callback) => {
       return p.then (callback);
-    }, Promise.resolve (content))
+    }, Promise.resolve (content));
   }
-  
+
 }
